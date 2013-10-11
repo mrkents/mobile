d130b4d9fe7fba75a77e3b241358b2d4678b26ca
mrkents@gmail.com <mrkents@gmail.com>
2013-10-10 19:54:44 -0500

    este es el primer cambio que hago

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ mobile
 ======
 
 Mobile Apps
+
+SenaApp
+Sena Applícate es la nueva forma de revisar los servicios más importantes del Servicio Nacional de Aprendizaje - SENA. 
---
+++ www/js/app.js
@@ -0,0 +1,61 @@
+/*
+  Todo el código JavaScript luego del DeviceReady !!
+ */
+function inicio(){
+	//navigator.notification.alert("Estamos listos..");
+	document.getElementById('load').style.display='none';
+	document.getElementById('app').style.display='block';
+	
+	// Register the event listener
+	document.addEventListener("menubutton", onMenuKeyDown, false);
+	document.addEventListener("backbutton", onBackKeyDown, false);
+	
+}
+
+// Handle the menu button
+function onMenuKeyDown() {
+	document.getElementById('menubutton').style.display='block';
+}
+
+// Handle the back button
+function onBackKeyDown() {
+	
+	if($.mobile.activePage.is('#home')){
+		if(document.getElementById('menubutton').style.display=='block'){
+			navigator.notification.alert("estaba abierto el menu");
+			document.getElementById('menubutton').style.display='none';
+			e.preventDefault();
+		}
+		else{
+			//navigator.notification.alert("salida normal por home");
+			navigator.app.exitApp();
+		}
+	}
+	/*else if($.mobile.activePage.is('#centros')){
+		navigator.app.backHistory()
+	}*/
+	else {
+		navigator.notification.alert("general back...");
+		navigator.app.backHistory()
+	}
+}
+
+
+/*
+function onDeviceReady() {
+      document.addEventListener("backbutton", handleBackButton, true);
+}
+function handleBackButton(){
+ 
+    if($.mobile.activePage.attr('id') == 'home'
+              || $.mobile.activePage.attr('id') == 'listaUsuarios'){
+        navigator.app.exitApp();
+    }else if ($.mobile.activePage.attr('id') == 'cita'){
+        $.mobile.changePage('#listaUsuarios');
+    }else{
+        navigator.app.backHistory();
+    }
+}
+ 
+document.addEventListener("deviceready", onDeviceReady, false);
+*/
